63fa52fe64b3831ec3d6b363784729664c4671bc
hnascx <henriquehusky@gmail.com>
2024-10-03 11:48:13 -0300

    feat/Dark theme configured

--- node_modules/docs/.storybook/preview.ts
+++ node_modules/docs/.storybook/preview.ts
@@ -2,6 +2,19 @@ import type { Preview } from "@storybook/react";
 
 const preview: Preview = {
   parameters: {
+    backgrounds: {
+      default: 'dark',
+      values: [
+        {
+          name: 'light',
+          value: '#F8F8F8',
+        },
+        {
+          name: 'dark',
+          value: '#333333',
+        }
+      ]
+    },
     controls: {
       matchers: {
         color: /(background|color)$/i,
