8c5d8eb2c360c3eef5ee423a1e41017f379006dc
hnascx <henriquehusky@gmail.com>
2024-10-04 11:07:47 -0300

    error/Could not resolve "entities"

--- packages/docs/.storybook/main.ts
+++ packages/docs/.storybook/main.ts
@@ -10,17 +10,27 @@ function getAbsolutePath(value: string): any {
   return dirname(require.resolve(join(value, "package.json")));
 }
 const config: StorybookConfig = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.tsx"],
+  stories: ["../src/pages/**/*.mdx", "../src/**/*.stories.tsx"],
+
   addons: [
     getAbsolutePath("@storybook/addon-onboarding"),
     getAbsolutePath("@storybook/addon-links"),
     getAbsolutePath("@storybook/addon-essentials"),
     getAbsolutePath("@chromatic-com/storybook"),
     getAbsolutePath("@storybook/addon-interactions"),
+    getAbsolutePath("@storybook/addon-mdx-gfm"),
+    "@chromatic-com/storybook"
   ],
+
   framework: {
     name: getAbsolutePath("@storybook/react-vite"),
     options: {},
   },
+
+  docs: {},
+
+  typescript: {
+    reactDocgen: "react-docgen-typescript"
+  }
 };
 export default config;
